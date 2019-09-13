29494e0533f358e34b549fe37e33e6f2c36c418a
Jeremy Vela <jvela924@gmail.com>
2019-09-13 10:27:56 -0400

    added header and handleview function

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -6,8 +6,19 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
+      view: {
+        page: 'home'
+      },
       foodtrucks: []
     }
+  }
+  handleView = (view) => {
+      // update the state
+      this.setState({
+          view: {
+            page: view
+          }
+      })
   }
   fetchFoodTrucks = () => {
     fetch('http://localhost:3001/')
@@ -24,7 +35,7 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <Header />
+        <Header handleView={this.handleView} />
       </div>
     )
   }
--- react-backend/client/src/Header.js
+++ react-backend/client/src/Header.js
@@ -4,6 +4,18 @@ class Header extends React.Component {
   render () {
     return (
       <div className = "header">
+        <nav>
+          <div className="header-title">
+            <h1 className="title">Rollin' Eats</h1>
+          </div>
+          <div className="buttons">
+            <button className="header-button" onClick={() => {this.props.handleView('home')}}>Home</button>
+            <button className="header-button" onClick={() =>
+            {this.props.handleView('all')}}>Find a Food Truck</button>
+            <button className="header-button" onClick={() =>
+            {this.props.handleView('profile')}}>Profile</button>
+          </div>
+        </nav>
       </div>
     )
   }
