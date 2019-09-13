886ad1abda7a9ec75f326ce03f65a736fc4f8351
Jeremy Vela <jvela924@gmail.com>
2019-09-13 19:10:08 -0400

    able to get random foodtrucks and random images

--- react-backend/client/src/Main.js
+++ react-backend/client/src/Main.js
@@ -24,10 +24,12 @@ class Main extends React.Component {
       console.log(this.state.randTruck);
       let randNum = Math.ceil(Math.random() * 6)
       randNum = randNum.toString()
-      const randPic = "pic" + randNum
+      const randPic = 'pic' + randNum
+      console.log(typeof randPic);
+
       this.setState({ randPic: randPic })
       console.log(this.state.foodtrucks[randTruck]);
-      const image = this.state.foodtrucks[randTruck].randPic
+      const image = this.state.foodtrucks[randTruck][randPic]
       this.setState({ image: image})
       console.log(this.state.image);
     })
@@ -50,7 +52,7 @@ class Main extends React.Component {
       <div className="main-div">
         <h1 className="main-h1">What do you want to eat?</h1>
         <div className="food-container">
-          <img className="like-dislike" src="https://www.nicepng.com/png/detail/170-1703148_red-sign-icon-mark-symbol-cross-marks-orange.png" alt="dislike" />
+          <img onClick={this.fetchFoodtrucks} className="like-dislike" src="https://www.nicepng.com/png/detail/170-1703148_red-sign-icon-mark-symbol-cross-marks-orange.png" alt="dislike" />
           <img className="main-image" src={this.state.image} alt={this.state.foodtruck.name} />
           <img className="like-dislike" src="https://www.pinclipart.com/picdir/big/200-2004257_heart-icon-red-heart-vector-free-clipart.png" alt="like" />
         </div>
