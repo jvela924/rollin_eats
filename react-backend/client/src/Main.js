dc2e32408ed30021f906031ede1192883b285f8d
Jeremy Vela <jvela924@gmail.com>
2019-09-13 12:31:34 -0400

    added styling for food container

--- react-backend/client/src/Main.js
+++ react-backend/client/src/Main.js
@@ -25,7 +25,11 @@ class Main extends React.Component {
     return (
       <div className="main-div">
         <h1 className="main-h1">What do you want to eat?</h1>
-        <img className="main-image" src={this.state.foodtruck.pic5} alt={this.state.foodtruck.name} />
+        <div className="food-container">
+          <img className="like-dislike" src="https://www.nicepng.com/png/detail/170-1703148_red-sign-icon-mark-symbol-cross-marks-orange.png" alt="dislike" />
+          <img className="main-image" src={this.state.foodtruck.pic5} alt={this.state.foodtruck.name} />
+          <img className="like-dislike" src="https://www.pinclipart.com/picdir/big/200-2004257_heart-icon-red-heart-vector-free-clipart.png" alt="like" />
+        </div>
       </div>
     )
   }
