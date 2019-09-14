4b03f064ba63e25799c2f83e5920ce9fa27a0fbd
Jeremy Vela <jvela924@gmail.com>
2019-09-14 14:56:40 -0400

    fixing some things

--- app.js
+++ app.js
@@ -22,25 +22,11 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/foodtrucks', foodtrucksRouter);
 app.use('/pics', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-app.get('/', (req, res) => {
-  res.render('index.html')
-});
+app.get('*', (req.res) => {
+  res.sendFile(path.join(_dirname+'/client/build/index.html'))
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 // Listener
 app.listen(port, () => console.log('Listening on port ${port}'));
