9a5c67ab74165572e037cc51ccd619aae5590bf4
Jeremy Vela <jvela924@gmail.com>
2019-09-14 13:12:33 -0400

    small change

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(function(req, res, next) {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build/public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
   });
 }
 
