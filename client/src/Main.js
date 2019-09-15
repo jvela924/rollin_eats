ff8f779451766195a52d1e762428922ea5a37240
Jeremy Vela <jvela924@gmail.com>
2019-09-15 09:51:20 -0400

    adding in like functionality

--- client/src/Main.js
+++ client/src/Main.js
@@ -49,20 +49,50 @@ class Main extends React.Component {
           <h1 className="main-h1">What do you want to eat?</h1>
           <div className="food-container">
             <img onClick={this.fetchFoodtrucks} className="like-dislike" src="https://www.nicepng.com/png/detail/170-1703148_red-sign-icon-mark-symbol-cross-marks-orange.png" alt="dislike" />
-            <img className="main-image" src={this.state.image} alt={this.state.foodtruck.name} />
+            <div className="food-div">
+              <img className="main-image" src={this.state.image} alt={this.state.foodtruck.name} />
+            </div>
             <img onClick={() => {this.props.handleView('liked')}} className="like-dislike" src="https://www.pinclipart.com/picdir/big/200-2004257_heart-icon-red-heart-vector-free-clipart.png" alt="like" />
           </div>
-        </div> :
+        </div> : ''}
 
+        {this.props.view.page ==='liked' ?
         <div>
           <h1>{this.state.foodtruck.name}</h1>
+          <h3>{this.state.foodtruck.description}</h3>
+          <h3>City: {this.state.foodtruck.city}</h3>
           <div className="social-media">
-            <a target="_blank" href={this.state.foodtruck.instagram}>Instagram</a>
-            <a target="_blank" href={this.state.foodtruck.twitter}>Twitter</a>
-            <a target="_blank" href={this.state.foodtruck.facebook}>Facebook</a>
+            <a class="fa fa-instagram" target="_blank" rel="noopener noreferrer" href={this.state.foodtruck.instagram}></a>
+            <a class="fa fa-twitter" rel="noopener noreferrer" target="_blank" href={this.state.foodtruck.twitter}></a>
+            <a class="fa fa-facebook" rel="noopener noreferrer" target="_blank" href={this.state.foodtruck.facebook}></a>
+          </div>
+          <div className="social-media">
+            <div className="truck-pics">
+              <img className="main-image" src={this.state.foodtruck.pic1} alt={this.state.foodtruck.name} />
+            </div>
+            <div className="truck-pics">
+              <img className="main-image" src={this.state.foodtruck.pic2} alt={this.state.foodtruck.name} />
+            </div>
+            <div className="truck-pics">
+              <img className="main-image" src={this.state.foodtruck.pic3} alt={this.state.foodtruck.name} />
+            </div>
+            <div className="truck-pics">
+              <img className="main-image" src={this.state.foodtruck.pic4} alt={this.state.foodtruck.name} />
+            </div>
+            <div className="truck-pics">
+              <img className="main-image" src={this.state.foodtruck.pic5} alt={this.state.foodtruck.name} />
+            </div>
+            <div className="truck-pics">
+              <img className="main-image" src={this.state.foodtruck.pic6} alt={this.state.foodtruck.name} />
+            </div>
+          </div>
+          <div className="back-home">
+            <button className="header-button" onClick={() => {this.props.handleView('home')}}>Find More Food</button>
           </div>
         </div>
-      }
+        : ''}
+
+        {this.props.view.page === 'all' ? <h1>Hello</h1> : ''}
       </div>
     )
   }
