c4574469ecf77109c29ea8ab72c49715cad0e154
Jeremy Vela <jvela924@gmail.com>
2019-09-19 11:55:56 -0400

    changed portfolio link

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
         <div>
           <a href="https://github.com/jvela924" className="fa fa-github" target="_blank"></a>
           <a className="fa fa-linkedin-square" href="https://www.linkedin.com/in/jeremy-vela/" target="_blank"></a>
-          <a className="portfolio" target="_blank" href="jeremyvela.com">jeremyvela.com</a>
+          <a className="portfolio" target="_blank" href="https://jvela924.github.io">jeremyvela.com</a>
         </div>
       </div>
     )
